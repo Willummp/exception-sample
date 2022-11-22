b94cc31b3a26ad7a8ffcc2e1e9af16989503ce43
Lucas Ferreira <lsousaah@gmail.com>
2022-11-22 09:17:36 -0300

    first commit

---
+++ src/script.ts
@@ -0,0 +1,23 @@
+class SystemError {
+  public message: string;
+  public type: string;
+  
+  constructor (message:string,type:string){
+    this.message = message;
+    this.type = type;
+  }
+} 
+console.log("Será que vai dar erro??");
+
+try {
+  console.log("Antes do erro");
+  throw new SystemError ("Erro que eu criei", "Abstrato");
+  console.log("Depois do erro");
+  } catch (error) { 
+    if (error instanceof SystemError){
+    console.log(`${error.type}: ${error.message}`);
+    } else {
+      console.log(`Error: ${error.message}`);
+    }
+  }
+console.log('cabooo');
